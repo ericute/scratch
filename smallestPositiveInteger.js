dd316887efc64838947d627ca7f48d94cd0ef13f
Eric Leron <ericjohnleron@gmail.com>
2019-07-10 00:00:49 +0800

    Solution for Demo Codility Exam in JavaScript

---
+++ smallestPositiveInteger.js
@@ -0,0 +1,20 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    
+    let sorted = A.sort();
+    let storage = {};
+    
+    // Store sorted Array in a json using the value as the key
+    for (let i = 0; i < sorted.length; i++) {
+        storage[sorted[i]] = sorted[i];
+    }
+    
+    // If the storage does not contain the key from the loop below, return the key
+    for (let i = 1; i < 1000001; i++) {
+        if (!storage[i]) return i; 
+    }
+    
+}
